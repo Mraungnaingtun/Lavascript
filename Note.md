03e45ef3a0340afb64c03d22c7e89c1798daa194
Aung Naing Tun <aungnaing.tun@partnerassociates.com>
2025-02-15 19:36:43 +0630

    study ts or js array

---
+++ Note.md
@@ -0,0 +1,5 @@
+1) install ts 
+     npm i typescript -g
+    
+2) Install ts-node package
+    npm i ts-node -g
---
+++ index.ts
@@ -0,0 +1,10 @@
+
+
+let arr1: Array<number> = [5, 10, 15, 20];
+let squareArray = arr1.map((x) => x * x);
+console.log(squareArray);
+
+let arr2 : number[] = [1, 2, 3, 4, 5, 6];
+let evenNumbers = arr2.filter(x => x % 2 === 0)
+console.log(evenNumbers)
+
